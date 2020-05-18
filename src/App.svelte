d8dd33465f016ccb63ce1937c4478999cd167547
Ole Eskild Steensen <oleeskilds@gmail.com>
2020-05-18 12:12:24 +0000

    Added default picture

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@ async function getWeeklyDinners(){
 	<h1>Dagens</h1>
 	{#await dinners then value}
 	<div class="dinner-info">
-		<Card dinnerName={value[currentWeekDay].dinner} weekDay={value[currentWeekDay].day} img={value[currentWeekDay].img} />	
+		<Card dinnerName={value[currentWeekDay].dinner} weekDay={value[currentWeekDay].day} img={value[currentWeekDay].img || "https://images.unsplash.com/photo-1547573854-74d2a71d0826?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"} />	
 	</div>
 	{/await}
 </main>
